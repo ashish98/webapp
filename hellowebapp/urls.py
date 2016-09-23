f9b23a861fb58f238db4be322d801c9904864be8
ashish98 <kumarashish2094@gmail.com>
2016-09-23 22:27:00 +0530

    v1.1

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, patterns, include
-from django.contrib import admin
+from django.contrib import admin, admin
+from django.views.generic import TemplateView
 
 """urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]"""
-urlpatterns = patterns('',url(r'^$','collection.views.index',name='home'),url(r'^admin/',include(admin.site.urls)),)
+urlpatterns = patterns('',url(r'^$','collection.views.index',name='home'),
+	url(r'^about/$', TemplateView.as_view(template_name = 'about.html'),name = 'about'),
+	url(r'^contact/$',TemplateView.as_view(template_name = 'contact.html'),name = 'contact'),
+	url(r'^admin/',include(admin.site.urls)),)
